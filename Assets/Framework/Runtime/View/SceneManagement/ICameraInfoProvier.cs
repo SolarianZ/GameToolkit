56db74609db9064de0ba4e4655c0c2305c311586
zqy <VDerGoW@hotmail.com>
2023-11-01 22:20:36 +0800

    feat: Scene management

---
+++ Assets/Framework/Runtime/View/SceneManagement/ICameraInfoProvier.cs
@@ -0,0 +1,15 @@
+﻿using System.Numerics;
+
+namespace GBG.Framework.View.SceneManagement
+{
+    /// <summary>
+    /// Provides information to determine which areas should be loaded.
+    /// </summary>
+    public interface ILoadSceneInfoProvier
+    {
+        Vector3 GetCameraPosition();
+        Vector3 GetCameraFocus();
+        float GetPreloadDistance();
+        float GetUnloadDistance();
+    }
+}
---
+++ Assets/Framework/Runtime/View/SceneManagement/IViewSceneManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace GBG.Framework.View.SceneManagement
+{
+    public interface IViewSceneManager
+    {
+        IReadOnlyList<int> LoadedScenes { get; }
+        IReadOnlyList<int> LoadingScenes { get; }
+        bool IsLoadingAnyScenes => LoadingScenes != null && LoadingScenes.Count > 0;
+
+        bool IsSceneLoaded(int SceneId) => LoadedScenes?.Contains(SceneId) ?? false;
+        bool IsSceneLoading(int SceneId) => LoadingScenes?.Contains(SceneId) ?? false;
+        void LoadScene(int SceneId);
+        void UnloadScene(int SceneId);
+    }
+}
