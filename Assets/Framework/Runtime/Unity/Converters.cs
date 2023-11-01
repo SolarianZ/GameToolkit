273a488d82cfdd673a827bf1b0f940de9b208700
zqy <VDerGoW@hotmail.com>
2023-11-01 21:11:45 +0800

    feat: Unity  Utilities

---
+++ Assets/Framework/Runtime/Unity/Converters.cs
@@ -0,0 +1,17 @@
+﻿#if UNITY_2022_1_OR_NEWER || GBG_FRAMEWORK_ENABLE_UNITY_APIS
+namespace GBG.Framework.Unity
+{
+    public static class Converters
+    {
+        public static System.Numerics.Vector2 ToSystem(this UnityEngine.Vector2 value) => new System.Numerics.Vector2(value.x, value.y);
+        public static System.Numerics.Vector3 ToSystem(this UnityEngine.Vector3 value) => new System.Numerics.Vector3(value.x, value.y, value.z);
+        public static System.Numerics.Vector4 ToSystem(this UnityEngine.Vector4 value) => new System.Numerics.Vector4(value.x, value.y, value.z, value.w);
+        public static System.Numerics.Quaternion ToSystem(this UnityEngine.Quaternion value) => new System.Numerics.Quaternion(value.x, value.y, value.z, value.w);
+
+        public static UnityEngine.Vector2 ToUnity(this System.Numerics.Vector2 value) => new UnityEngine.Vector2(value.X, value.Y);
+        public static UnityEngine.Vector3 ToUnity(this System.Numerics.Vector3 value) => new UnityEngine.Vector3(value.X, value.Y, value.Z);
+        public static UnityEngine.Vector4 ToUnity(this System.Numerics.Vector4 value) => new UnityEngine.Vector4(value.X, value.Y, value.Z, value.W);
+        public static UnityEngine.Quaternion ToUnity(this System.Numerics.Quaternion value) => new UnityEngine.Quaternion(value.X, value.Y, value.Z, value.W);
+    }
+}
+#endif
---
+++ Assets/Framework/Runtime/Unity/MonoSingleton.cs
@@ -0,0 +1,49 @@
+﻿#if UNITY_2022_1_OR_NEWER || GBG_FRAMEWORK_ENABLE_UNITY_APIS
+using UnityEngine;
+
+namespace GBG.Framework.Unity
+{
+    [DisallowMultipleComponent]
+    public abstract class MonoSingleton<T> : MonoBehaviour where T : MonoSingleton<T>
+    {
+        private static T _instance;
+
+
+        public static T GetInstance(bool createIfNotExists = false, string newInstanceName = null)
+        {
+            if (!_instance && createIfNotExists)
+            {
+                newInstanceName ??= $"[{typeof(T).Name}]";
+                var go = new GameObject(newInstanceName);
+                go.AddComponent<T>();
+            }
+
+            return _instance;
+        }
+
+
+        protected bool IsValidInstance { get; private set; }
+
+
+        #region Unity Messages
+
+        protected virtual void Awake()
+        {
+            if (!_instance)
+            {
+                _instance = (T)this;
+                DontDestroyOnLoad(this);
+                IsValidInstance = true;
+            }
+            else
+            {
+                enabled = false;
+                IsValidInstance = false;
+                Debug.LogError($"There is already another {typeof(T).Name} instance, disable this one({this}).", this);
+            }
+        }
+
+        #endregion
+    }
+}
+#endif
