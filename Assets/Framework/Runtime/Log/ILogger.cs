e224063f6ea1857f58f83a2766ba79fc9fd13ec1
zqy <VDerGoW@hotmail.com>
2023-11-02 20:53:10 +0800

    feat: Log level & Log format

--- Assets/Framework/Runtime/Log/ILogger.cs
+++ Assets/Framework/Runtime/Log/ILogger.cs
@@ -1,21 +1,30 @@
 ﻿namespace GBG.Framework.Log
 {
-    public enum LogType
+    public enum LogLevel
     {
+        Trace = 0,
         Debug,
         Info,
         Warn,
         Error,
         Fatal,
+        Off,
     }
 
     public interface ILogger
     {
-        void Log(LogType type, string tag, string message, object context);
+        // NLog format: "${datetime}|${LEVEL}|${logger}|${message}"
+        // Log4net format: "%timestamp [%thread] %-5level %logger - %message"
+
+        void Log(LogLevel level, string tag, string message, object context);
+        //[System.Diagnostics.Conditional("GBG_FRAMEWORK_LOG_TRACE")]
+        void LogTrace(string message, object context = null, string tag = null);
         //[System.Diagnostics.Conditional("GBG_FRAMEWORK_LOG_DEBUG")]
         void LogDebug(string message, object context = null, string tag = null);
         void LogInfo(string message, object context = null, string tag = null);
         void LogWarn(string message, object context = null, string tag = null);
+        //[System.Diagnostics.Conditional("GBG_FRAMEWORK_LOG_ASSERT")]
+        void LogAssert(bool condition, string message, object context = null, string tag = null);
         void LogError(string message, object context = null, string tag = null);
         void LogFatal(string message, object context = null, string tag = null);
     }
