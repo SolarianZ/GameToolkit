cdcabacbefe1d02ba8e4298f24577fe5135be4a6
zqy <VDerGoW@hotmail.com>
2023-11-04 00:14:56 +0800

    feat: Pipeline

---
+++ Assets/Framework/Runtime/Pipeline/IComplexPipeline.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace GBG.Framework.Process
+{
+    public interface IComplexPipelineView : IPipelineView, IEnumerable<IPipelineView>
+    {
+        int DirectSubPipelineCount { get; }
+        IPipelineView this[int index] { get; }
+    }
+
+    public interface IComplexPipeline : IPipeline, IComplexPipelineView
+    {
+        bool ContainsSubPipeline(IPipeline pipeline);
+        void AddSubPipeline(IPipeline pipeline);
+        bool TryAddSubPipeline(IPipeline pipeline);
+        bool RemoveSubPipeline(IPipeline pipeline);
+        void ClearSubPipelines();
+    }
+}
---
+++ Assets/Framework/Runtime/Pipeline/IPipeline.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace GBG.Framework.Process
+{
+    // Pipeline Stage Step
+
+    public enum PipelineState
+    {
+        NotStarted,
+        Running,
+        Paused,
+        Canceled,
+        Completed,
+    }
+
+    public interface IPipelineView
+    {
+        int Id { get; }
+        string Name { get; }
+        int Priority { get; }
+        float Progress { get; }
+        //int Stage { get; }
+        PipelineState State { get; }
+
+        event Action<IPipelineView> Started;
+        event Action<IPipelineView> Paused;
+        event Action<IPipelineView> Resumed;
+        event Action<IPipelineView> Canceled;
+        event Action<IPipelineView> Completed;
+        event Action<IPipelineView, PipelineState, PipelineState> StateChanged;
+    }
+
+    public interface IPipeline : IPipelineView
+    {
+        void Start();
+        void Pause();
+        void Resume();
+        void Cancel();
+    }
+}
