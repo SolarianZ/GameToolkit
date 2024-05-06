2c1b51e981220eb805542327d8b4211e745bad3a
zqy <VDerGoW@hotmail.com>
2024-05-06 22:40:17 +0800

    feat: Add default asset providers for asset checker

---
+++ Editor/AssetChecker/ExplicitAssetProvider.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UObject = UnityEngine.Object;
+
+namespace GBG.GameToolkit.Unity.Editor.AssetChecker
+{
+    [CreateAssetMenu(menuName = "Bamboo/Asset Checker/Explicit Asset Provider")]
+    public class ExplicitAssetProvider : AssetProvider
+    {
+        public UObject[] assets = Array.Empty<UObject>();
+
+
+        public override IReadOnlyList<UObject> GetAssets()
+        {
+            return assets;
+        }
+    }
+}
