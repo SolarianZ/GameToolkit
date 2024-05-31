fa94ff87c496660e7ab3b134b1a3019dae37adaf
ZQY <VDerGoW@hotmail.com>
2024-05-31 20:45:26 +0800

    feat: Int32EnumIdConfig<T>

--- Runtime/ConfigData/IConfig.cs
+++ Runtime/ConfigData/IConfig.cs
@@ -1,7 +1,36 @@
-﻿namespace GBG.GameToolkit.ConfigData
+﻿using System;
+
+namespace GBG.GameToolkit.ConfigData
 {
     public interface IConfig
     {
         public int Id { get; }
+    }
+
+    public interface IInt32EnumIdConfig<T> : IConfig where T : Enum
+    {
+        T EnumId { get; }
+    }
+
+    [Serializable]
+    public abstract class Int32EnumIdConfig<T> : IInt32EnumIdConfig<T> where T : Enum
+    {
+        /// <inheritdoc />
+        int IConfig.Id
+        {
+            get
+            {
+#if UNITY_EDITOR
+                // Value may be modified from Inspector, do not cache
+                SetEnumValueCacheDirty();
+#endif
+                _enumValueCache ??= ((IConvertible)EnumId).ToInt32(null);
+                return _enumValueCache.Value;
+            }
+        }
+        public abstract T EnumId { get; }
+        private int? _enumValueCache;
+
+        public void SetEnumValueCacheDirty() => _enumValueCache = null;
     }
 }
